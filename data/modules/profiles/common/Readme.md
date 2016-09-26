e161281970c0cc73bd166249c94da6ce61d063f6
obi11235 <paul@frozenphoenix.net>
2016-09-26 17:42:02 -0400

    handling of files

--- data/modules/profiles/common/Readme.md
+++
@@ -1,3 +0,0 @@
-#Data for all nodes
-
-Since our profiles class contains many sub-classes, we've broken out each sub-class into its own file.
--- modules/profiles/manifests/config_files.pp
+++ modules/profiles/manifests/config_files.pp
@@ -1,15 +1,58 @@
-class profiles::config_files{
+class profiles::config_files(
+	$num_processes = $profiles::config_files::params::num_processes,
+  $interfaces = $profiles::config_files::params::interfaces,
+  ) inherits profiles::config_files::params{
 
-  file { "/ect/config-profile" :
+# Creating a directory and setting it's ownership / mode
+  file { "/etc/config-profile" :
     ensure => 'directory',
     owner => 'root',
     group => 'root',
     mode => "2775"
   } ->
-  file { "/etc/config-profile/tempitized.conf" :
-    content => template('profiles/config_files/template.conf.erb'),
+
+# Creating a directory and copying all files in a directory in the repo into the dir
+  file { "/etc/config-profile/bind" :
+    ensure => 'directory',
+    owner => 'root',
+    group => 'root',
+    mode => "2775",
+    recurse => true,
+    source => 'puppet:///modules/profiles/config_files/bind',
+  } ->
+
+# sym link to another file
+file { "/etc/config-profile/bind/active.conf" :
+    ensure => link,
+    target => '/etc/config-profile/bind/file_b.conf'
+    }->
+
+# Hard coded file
+file { "/etc/config-profile/hard_coded.conf" :
+    content => 'Hard coded content for the file goes here
+    look, multiple lines',
+    ensure => present,
     owner => "root",
     group => "root",
     mode => "0440",
+    }->
+
+# Templitized config file
+file { "/etc/config-profile/tempitized.conf" :
+    content => template('profiles/config_files/template.conf.erb'),
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => "0440",
+    } ->
+  
+# remove a file does not exist
+file { "/etc/config-profile/empty-file" :
+    ensure => absent,
+    }-> 
+
+file { "/etc/config-profile/empty-dir" :
+    ensure => absent,
+    force => true,
     }
 }
