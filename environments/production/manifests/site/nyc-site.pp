520ef1fbe11ffa3fab7cbe829ef54fe7491f80ca
Martin Beauchamp <martinb@shapeways.com>
2016-09-26 10:03:31 -0400

    Updated site.pp.

--- environments/production/manifests/site/nyc-site.pp
+++ environments/production/manifests/site/nyc-site.pp
@@ -2,10 +2,10 @@
 
 #Do not add `default` node here, add it to site.pp
 
-#Catch-all for nyc.shapeways.net
-node /nyc.shapeways.net$/ {
-}
-
 node 'example.nyc.shapeways.net' {
   include profiles::example
+}
+
+#Catch-all for nyc.shapeways.net
+node /nyc.shapeways.net$/ {
 }
--- environments/production/manifests/site/site.pp
+++ environments/production/manifests/site/site.pp
@@ -2,13 +2,20 @@
 
 #set up global $idc variable
 if $fqdn =~ /nyc.shapeways.net$/ {
+    $provider = 'sw'
     $dc = 'nyc'
     notice("Welcome to NYC!")
+} elsif $hostname =~ /-aws1a-euwest$/ {
+    $dc = 'aws1a-euwest'
+    $provider = 'aws'
+    notice("Welcome to AWS EU West 1a!")
 } elsif $hostname =~ /-lab$/ {
+    $provider = 'sw'
     $dc = 'lab'
     notice("Welcome to DEBUG!")
 } else {
     $dc = ''
+    $provider = ''
 }
 
 #Global default
